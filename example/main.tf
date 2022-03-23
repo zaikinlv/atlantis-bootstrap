39b975aab21ae141bd1cc00caa7980fb950214ba
Stenbrenden, Are <Are.Stenbrenden@ssb.no>
2022-03-23 12:22:46 +0100

    Update examples
    Update examples to reflect changes needed for cert-manager version
    1.7.1, and additional clean up of the examples.
    
    Internal-ref: [STRATUS-998]
    
    Co-authored-by: ssbdif <35797607+ssbdif@users.noreply.github.com>

--- example/main.tf
+++ example/main.tf
@@ -22,4 +22,5 @@ module "atlantis-test" {
   azure_subscription_id = var.azure_subscription_id
   azure_tenant_id       = var.azure_tenant_id
   azure_client_id       = var.azure_client_id
+  external_ip_name      = "atlantis-external-facing-ip"
 }
